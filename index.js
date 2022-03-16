ba5b2b10804adbbcc8d2ef84c8d76f6cd8e1f463
Klab93 <kylian.bourcoud@gmail.com>
2022-03-16 14:20:45 +0100

    little server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello");
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+fs.writeFile('email.txt', 'List of email address\n ============', (err) => {
+    if (err) return console.log(err)
+    console.log("email.txt created")
+})
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+
+    fs.appendFile('email.txt', `\n${req.body.email}`, (err) => {
+        if (err) return console.log(err)
+        console.log("done")
+    })
+    res.send("register email address!")
+})
+
+app.listen(3000)
